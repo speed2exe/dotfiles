52661f34bf8936527d97c81f64ceb28010a9a8c6
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2021-10-23 00:30:40 +0800

    use newer lualine, take back old lspsaga for compatibility, separate treesitter-refactor

--- nvim/lua/lualine_conf.lua
+++ nvim/lua/lualine_conf.lua
@@ -1,9 +1,15 @@
 require'lualine'.setup {
     options = {
-        icons_enabled = true,
-        theme = 'moonfly',
-        component_separators = {'', ''},
-        section_separators = {'', ''},
+        --theme = 'moonfly',
+        theme = 'auto',
+        component_separators = {
+            left = '', 
+            right = '',
+        },
+        section_separators = {
+            left = '',
+            right = '',
+        },
         disabled_filetypes = {}
     },
     sections = {
@@ -14,15 +20,7 @@ require'lualine'.setup {
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
     tabline = {},
-    extensions = {}
+    extensions = {'nvim-tree'}
 }
 
