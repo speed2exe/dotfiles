3578e0c904dc20cbea472af3893915f46343c7c9
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2021-11-08 22:02:28 +0800

    add to starship

--- save.fish
+++ save.fish
@@ -3,9 +3,11 @@ git stash
 rm -r nvim/
 rm -r fish/
 rm -r kitty/
+rm starship.toml
 
 cp -r ~/.config/fish/ .
 cp -r ~/.config/nvim/ .
 cp -r ~/.config/kitty/ .
+cp ~/.config/starship.toml
 
 git diff
