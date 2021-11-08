519728bbfeb6cf8e62d8108a3f6878f77c8b684b
Fu Zi Xiang <zi.fu@bytedance.com>
2021-11-08 22:02:28 +0800

    add to starship

--- load.fish
+++ load.fish
@@ -1,8 +1,10 @@
 rm -rf ~/.config/nvim/
 rm -rf ~/.config/fish/
 rm -rf ~/.config/kitty/
+rm -f ~/.config/starship.toml
 
 cp -r nvim/ ~/.config/nvim/
 cp -r fish/ ~/.config/fish/
 cp -r kitty/ ~/.config/kitty/
+cp starship.toml ~/.config/starship.toml
 
