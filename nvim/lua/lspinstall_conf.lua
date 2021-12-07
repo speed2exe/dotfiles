d759cff2688f417fb2b0011a156a65c2200850a5
Fu Zi Xiang <zi.fu@bytedance.com>
2021-12-07 14:02:42 +0800

    added lsp status to lualine

--- nvim/lua/lspinstall_conf.lua
+++ nvim/lua/lspinstall_conf.lua
@@ -1,6 +1,15 @@
 local lspconfig = require('lspconfig')
+local lsp_status = require('lsp-status')
+
+lsp_status.register_progress()
+
 require'lspinstall'.setup() -- important
 local servers = require'lspinstall'.installed_servers()
 for _, server in pairs(servers) do
-    lspconfig[server].setup({})
+    lspconfig[server].setup({
+        handers = lsp_status.extensions[server].setup(),
+        on_attach = lsp_status.on_attach,
+        capabilities = lsp_status.capabilities,
+    })
 end
+
--- nvim/lua/lualine_conf.lua
+++ nvim/lua/lualine_conf.lua
@@ -1,8 +1,9 @@
 require'lualine'.setup {
     options = {
+        icons_enabled = true,
         theme = 'auto',
         component_separators = {
-            left = '', 
+            left = '',
             right = '',
         },
         section_separators = {
@@ -13,8 +14,13 @@ require'lualine'.setup {
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
+        lualine_b = {
+            'branch', 'diff', {'diagnostics', sources={'nvim_lsp'}},
+        },
+        lualine_c = {
+            'filename',
+            'LspStatus',
+        },
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
