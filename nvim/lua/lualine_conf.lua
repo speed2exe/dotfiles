fefdbd05ce7dda04e2e8671271b2f082bcfd329b
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2021-12-11 23:40:22 +0800

    update lualine

--- nvim/lua/lualine_conf.lua
+++ nvim/lua/lualine_conf.lua
@@ -15,7 +15,7 @@ require'lualine'.setup {
     sections = {
         lualine_a = {'mode'},
         lualine_b = {
-            'branch', 'diff', {'diagnostics', sources={'nvim_lsp'}},
+            'branch', 'diff', {'diagnostics', sources={'nvim_diagnostic'}},
         },
         lualine_c = {
             'filename',
