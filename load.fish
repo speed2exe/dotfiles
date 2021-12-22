dc08e13299d7deca03072667d14afdf5f12032e1
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2021-12-23 02:19:44 +0800

    add neofetch save and load

--- load.fish
+++ load.fish
@@ -1,10 +1,12 @@
 rm -rf ~/.config/nvim/
 rm -rf ~/.config/fish/
 rm -rf ~/.config/kitty/
+rm -rf ~/.config/neofetch/
 rm -f ~/.config/starship.toml
 
 cp -r nvim/ ~/.config/nvim/
 cp -r fish/ ~/.config/fish/
 cp -r kitty/ ~/.config/kitty/
+cp -r neofetch/ ~/.config/neofetch/
 cp starship.toml ~/.config/starship.toml
 
--- save.fish
+++ save.fish
@@ -3,11 +3,13 @@ git stash
 rm -r nvim/
 rm -r fish/
 rm -r kitty/
+rm -r neofetch/
 rm starship.toml
 
 cp -r ~/.config/fish/ .
 cp -r ~/.config/nvim/ .
 cp -r ~/.config/kitty/ .
+cp -r ~/.config/neofetch/ .
 cp ~/.config/starship.toml .
 
 git diff
