56c8135f435b52df356fa7190cfb3e3c82cd1c5a
Fu Zi Xiang <speed2exe@live.com.sg>
2021-12-26 12:44:20 +0800

    added zip for extract

--- fish/config.fish
+++ fish/config.fish
@@ -391,8 +391,19 @@ function extract
             uncompress $path; return
         case '*.7z'
             7z x $path; return
+        case '*.zip'
+            unzip $path; return
         case '*'
             echo "unknown file extension for extraction"
             return 1
     end
+end
+
+# zip
+function mkzip
+    if test (count $argv) -ne 1
+        echo "requires 1 arg: path to file/folder"
+    end
+
+    commandline "zip -vr $argv.zip $argv"
 end
