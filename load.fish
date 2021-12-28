5ff7661d4c0c829361c114f29d014901d4fbe386
Fu Zi Xiang <speed2exe@live.com.sg>
2021-12-28 14:39:17 +0800

    add save and load for fonst

--- load.fish
+++ load.fish
@@ -2,12 +2,14 @@ rm -rf ~/.config/nvim/
 rm -rf ~/.config/fish/
 rm -rf ~/.config/kitty/
 rm -rf ~/.config/neofetch/
+rm -rf ~/.config/fonts/
 rm -f ~/.config/starship.toml
 
 cp -r nvim/ ~/.config/nvim/
 cp -r fish/ ~/.config/fish/
 cp -r kitty/ ~/.config/kitty/
 cp -r neofetch/ ~/.config/neofetch/
+cp -r fonts/ ~/.config/fonts/
 cp starship.toml ~/.config/starship.toml
 
 
--- save.fish
+++ save.fish
@@ -1,15 +1,17 @@
 git stash
 
-rm -r nvim/
 rm -r fish/
+rm -r nvim/
 rm -r kitty/
 rm -r neofetch/
+rm -r fonts/
 rm starship.toml
 
 cp -r ~/.config/fish/ .
 cp -r ~/.config/nvim/ .
 cp -r ~/.config/kitty/ .
 cp -r ~/.config/neofetch/ .
+cp -r ~/.config/fonts/ .
 cp ~/.config/starship.toml .
 
 git diff
