4a98e121ffb29208b287b0b79f8799d268f88ad0
Fu Zi Xiang <speed2exe@live.com.sg>
2021-12-28 15:12:42 +0800

    add font configs for loading and saving

--- load.fish
+++ load.fish
@@ -3,6 +3,7 @@ rm -rf ~/.config/fish/
 rm -rf ~/.config/kitty/
 rm -rf ~/.config/neofetch/
 rm -rf ~/.config/fonts/
+rm -rf ~/.config/fontconfig/
 rm -f ~/.config/starship.toml
 
 cp -r nvim/ ~/.config/nvim/
@@ -10,6 +11,7 @@ cp -r fish/ ~/.config/fish/
 cp -r kitty/ ~/.config/kitty/
 cp -r neofetch/ ~/.config/neofetch/
 cp -r fonts/ ~/.config/fonts/
+cp -r fonts/ ~/.config/fontconfig/
 cp starship.toml ~/.config/starship.toml
 
 
