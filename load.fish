8f6a9fa03f36124107ac5fd1b7bd5c35b6cb9541
Fu Zi Xiang <speed2exe@live.com.sg>
2022-01-02 01:43:51 +0800

    update load script

--- load.fish
+++ load.fish
@@ -1,19 +1,10 @@
-rm -rf ~/.config/nvim/
-rm -rf ~/.config/fish/
-rm -rf ~/.config/kitty/
-rm -rf ~/.config/neofetch/
-rm -rf ~/.config/fonts/
-rm -rf ~/.config/fontconfig/
-rm -rf ~/.config/alacritty/
+set folder_names fish nvim fish kitty neofetch fonts fontconfig \
+    alacritty bspwm polybar sxhkd rofi
+
+for name in $folder_names
+    rm -rf ~/.config/$name/
+    cp -r $name/ ~/.config/
+end
+
 rm -f ~/.config/starship.toml
-
-cp -r nvim/ ~/.config/nvim/
-cp -r fish/ ~/.config/fish/
-cp -r kitty/ ~/.config/kitty/
-cp -r neofetch/ ~/.config/neofetch/
-cp -r fonts/ ~/.config/fonts/
-cp -r fonts/ ~/.config/fontconfig/
-cp -r alacritty/ ~/.config/alacritty/
 cp starship.toml ~/.config/starship.toml
-
-
