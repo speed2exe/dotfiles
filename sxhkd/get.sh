0980421dbe94e0b8efc6c24c86599c942a90651d
Fu Zi Xiang <speed2exe@live.com.sg>
2022-01-05 01:32:01 +0800

    general fixes and update

--- sxhkd/get.sh
+++ sxhkd/get.sh
@@ -2,4 +2,11 @@
 
 desk=$(bspc query -D -d)
 rofi -show combi -show-icons -sidebar-mode -steal-focus
+
+desk_then=$(bspc query -D -d)
+if [ $desk_then != $desk ]
+then
+    bspc node -i
+fi
+
 bspc node -d $desk --follow
