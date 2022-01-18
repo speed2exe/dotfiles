4b14deae5a92c953016741a3abdd6ddce64a08a5
Fu Zi Xiang <speed2exe@live.com.sg>
2022-01-18 22:12:46 +0800

    new desktop menu does not have 'Type to filter'

--- sxhkd/rename_desktop.sh
+++ sxhkd/rename_desktop.sh
@@ -1,4 +1,4 @@
-new_name=$(rofi -steal-focus -l 0 -p 'new desktop name' -dmenu -theme-str 'window {width:10%;}')
+new_name=$(rofi -steal-focus -l 0 -p 'new desktop name' -dmenu -theme-str 'window {width:10%;}entry{placeholder:"";}inputbar{children:[prompt,textbox-prompt-colon,entry];}')
 if [ $new_name ]
 then
     bspc desktop -n "[$new_name]"
