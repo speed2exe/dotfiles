51902e9215d16e37f27a6e713675586aaf3bad1b
Fu Zi Xiang <speed2exe@live.com.sg>
2022-03-09 18:43:48 +0800

    rework asroot and nvim ZZ

--- fish/functions/asroot.fish
+++ fish/functions/asroot.fish
@@ -1,3 +1,3 @@
 function asroot --wraps='set last (history -n 1) && commandline "doas $last"' --description 'alias asroot set last (history -n 1) && commandline "doas $last"'
-  set last (history -n 1) && commandline "doas $last" $argv; 
+  set last (history -n 1) && fish -c "doas $last $argv";
 end
