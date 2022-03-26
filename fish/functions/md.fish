f9abfd43dcf10d93bb66e474ff322cabef2292f8
Fu Zi Xiang <speed2exe@live.com.sg>
2022-03-27 02:16:48 +0800

    rework past files open and recording

--- fish/functions/md.fish
+++
@@ -1,3 +0,0 @@
-function md --wraps='nvim ~/marks/directories.txt' --description 'alias md nvim ~/marks/directories.txt'
-  nvim ~/marks/directories.txt $argv; 
-end
--- fish/functions/mf.fish
+++
@@ -1,3 +0,0 @@
-function mf --wraps='nvim ~/marks/files.txt' --description 'alias mf nvim ~/marks/files.txt'
-  nvim ~/marks/files.txt $argv; 
-end
