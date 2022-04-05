57e4158c2259c757955b1f07ed8fcac317f7ef79
Fu Zi Xiang <speed2exe@live.com.sg>
2022-04-06 00:28:00 +0800

    add fish clipboard support && temporary solution for commandline not working as expected

--- fish/functions/h.fish
+++ fish/functions/h.fish
@@ -1,3 +1,3 @@
-function h --wraps='commandline (history | fr --no-sort)' --description 'alias h commandline (history | fr --no-sort)'
-  commandline (history | fr --no-sort) $argv; 
+function h
+  commandline (history | fr --no-sort) $argv
 end
