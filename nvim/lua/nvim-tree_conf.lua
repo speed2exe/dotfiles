1cfb9eddb6f7b14b07cf205194da6b5260e453c2
Fu Zi Xiang <speed2exe@live.com.sg>
2022-04-06 12:29:24 +0800

    update nvim tree

--- nvim/lua/nvim-tree_conf.lua
+++ nvim/lua/nvim-tree_conf.lua
@@ -35,26 +35,49 @@ local list = {
 }
 
 require'nvim-tree'.setup {
-    disable_netrw        = true,
-    hijack_netrw         = true,
-    open_on_setup        = false,
-    ignore_ft_on_setup   = {},
-    auto_close           = true,
     auto_reload_on_write = true,
-    open_on_tab          = true,
-    update_to_buf_dir    = {
-        enable = true,
-        auto_open = true,
-    },
-    hijack_cursor       = true,
-    update_cwd          = false,
+    disable_netrw = true,
+    hide_root_folder = false,
+    hijack_cursor = true,
+    hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = true,
+    sort_by = "name",
+    update_cwd = false,
+    view = {
+        width = 30,
+        height = 30,
+        side = 'left',
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+            custom_only = true,
+            list = list,
+        },
+        -- auto_resize = true,
+    },
     hijack_directories   = {
         enable = true,
         auto_open = true,
     },
+    update_focused_file = {
+        enable = true,
+        update_cwd  = true,
+        ignore_list = {},
+    },
+    ignore_ft_on_setup   = {},
+    system_open = {
+        cmd  = "open",
+        args = {}
+    },
     diagnostics = {
         enable = true,
+        show_on_dirs = true,
         icons = {
             hint = "",
             info = "",
@@ -62,42 +85,15 @@ require'nvim-tree'.setup {
             error = "",
         }
     },
-    update_focused_file = {
-        enable = true,
-        update_cwd  = true,
-        ignore_list = {},
-    },
-    system_open = {
-        cmd  = "open",
-        args = {}
-    },
     filters  = {
         dotfiles = false,
-        custome = {}
+        custom = {},
+        exclude = {},
     },
     git = {
         enable = true,
         ignore = true,
         timeout = 1000,
-    },
-    view = {
-        width = 30,
-        height = 30,
-        hide_root_folder = false,
-        side = 'left',
-        auto_resize = true,
-        preserve_window_proportions = false,
-        mappings = {
-            custom_only = true,
-            list = list
-        },
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes"
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true
     },
     actions = {
         change_dir = {
@@ -110,13 +106,30 @@ require'nvim-tree'.setup {
                 enable = true,
                 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
                 exclude = {
-                    filetype = {
-                        "notify",
-                        "packer",
-                        "qf"
-                    }
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
                 }
             }
         }
-    }
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true
+    },
+    log = {
+        enable = false,
+        truncate = false,
+        types = {
+            all = false,
+            config = false,
+            copy_paste = false,
+            git = false,
+            profile = false,
+        },
+    },
+
+    -- update_to_buf_dir    = {
+    --     enable = true,
+    --     auto_open = true,
+    -- },
 }
