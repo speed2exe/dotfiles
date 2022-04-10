b76f105bf8ecf8e3a46803f1830c1ccfe59ccd76
Fu Zi Xiang <speed2exe@live.com.sg>
2022-04-10 22:30:45 +0800

    rework f.fish and copy

--- fish/functions/copy.fish
+++ fish/functions/copy.fish
@@ -1,7 +1,8 @@
 function copy
-  if [ (count $argv) -eq 0 ]
-    read -z > ~/.clipboard
-  else
-    $argv > ~/.clipboard
-  end
+    if [ (count $argv) -eq 0 ]
+        read -z > /tmp/.clipboard
+        xclip -sel clip /tmp/.clipboard
+    else
+        $argv | copy
+    end
 end
