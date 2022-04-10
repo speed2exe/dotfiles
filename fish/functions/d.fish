40a06fbad69c6fe6f0f8f2923ed47fb7631d8e13
Fu Zi Xiang <speed2exe@live.com.sg>
2022-04-10 23:41:02 +0800

    rework d.fish

--- fish/functions/d.fish
+++ fish/functions/d.fish
@@ -1,13 +1,15 @@
 function d
     if [ (count $argv) -eq 0 ]
         set path (pwd)
-    else if [ -d $argv ]
+    else if test -d $argv
         set path (get_dir $argv)
+    else if test -f $argv
+        trash $argv
+        return
     end
 
     set argv (fd_all --exact-depth 1 . $path | fpr --query "$path/")
-
-    if [ $argv ]
-        commandline "trash $argv"
+    if test $status -eq 0
+        trash $argv
     end
 end
