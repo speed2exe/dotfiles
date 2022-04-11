8d518d1fab1531815671561ffff3ffb25f2191b0
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-04-11 10:15:02 +0800

    added initial save.fish for darwin

---
+++ darwin/save.fish
@@ -0,0 +1,15 @@
+set folder_names (cat folders.txt)
+
+git stash
+
+for name in $folder_names
+    rm -rf ../$name
+    cp -r ~/.config/$name/ ..
+end
+
+rm ../starship.toml
+cp ~/.config/starship.toml ..
+
+git add .
+git diff --cached
+git status
