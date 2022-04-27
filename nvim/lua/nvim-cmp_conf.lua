ef0b2607524a87ffcaedf61c53ae3d18302d0149
Fu Zi Xiang <speed2exe@live.com.sg>
2022-04-28 02:44:04 +0800

    format cmp.setup.cmdline

--- nvim/lua/nvim-cmp_conf.lua
+++ nvim/lua/nvim-cmp_conf.lua
@@ -54,19 +54,17 @@ cmp.setup({
 })
 
 cmp.setup.cmdline('/', {
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp_document_symbol' }
-	}, {
-			{ name = 'buffer' }
-		})
+	sources = cmp.config.sources(
+		{ { name = 'nvim_lsp_document_symbol' } },
+		{ { name = 'buffer' } }
+	)
 })
 
 cmp.setup.cmdline(':', {
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-			{ name = 'cmdline' }
-		})
+	sources = cmp.config.sources(
+		{ { name = 'path' } }, 
+		{ { name = 'cmdline' } }
+	)
 })
 
 -- Setup lspconfig.
