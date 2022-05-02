d92b2fd1ddc1c374dfa2be5a05a8d509e2e012db
Fu Zi Xiang <speed2exe@live.com.sg>
2022-05-03 02:05:38 +0800

    add alt action to pull desktop

--- sxhkd/rename_desktop.sh
+++ sxhkd/rename_desktop.sh
@@ -1,5 +1,5 @@
 new_name=$(rofi -steal-focus -l 0 -p 'new desktop name' -dmenu -theme-str 'window {width:10%;}entry{placeholder:"";}inputbar{children:[prompt,textbox-prompt-colon,entry];}')
-if [ $new_name ]
+if [ "$new_name" ]
 then
     bspc desktop -n "[$new_name]"
 fi
