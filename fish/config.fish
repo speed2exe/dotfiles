d198ef6869bef4d19f03a57d150f2cc5916996f0
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-05-17 01:12:26 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -25,10 +25,10 @@ set STARSHIP_CONFIG "~/.config/starship.toml"
 fish_vi_key_bindings
 function fish_mode_prompt
 	switch $fish_bind_mode
-	case default
-		echo -en "\e[2 q"
-	case insert
+	case "insert"
 		echo -en "\e[6 q"
+	case "*"
+		echo -en "\e[2 q"
 	end
 end
 
