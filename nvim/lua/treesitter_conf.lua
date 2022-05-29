39ff50d49b8cd9781a458a3f0e43243eb74d0a56
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-05-29 22:26:48 +0800

    remove phpdoc from treesitter install list

--- nvim/lua/treesitter_conf.lua
+++ nvim/lua/treesitter_conf.lua
@@ -1,8 +1,10 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     sync_install = true,
+	ignore_install = {"phpdoc"},
     highlight = {
         enable = true,
+		disable = {},
         additional_vim_regex_highlighting = true,
     },
     indent = {
