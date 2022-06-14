42e1b4877f27879507fc31143f38e488ce897ff4
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-15 01:28:35 +0800

    update macos

--- macos/pre_config.fish
+++ macos/pre_config.fish
@@ -2,13 +2,15 @@
 fish_add_path "/opt/homebrew/bin"
 
 # node@16
-set -l node_16 (ls "/opt/homebrew/Cellar/node@16/")
-if test -n "$node_16"
-	fish_add_path "$node_16[1]/bin"
+set -l node_16_path "/opt/homebrew/Cellar/node@16/"
+set -l node_16_versions (ls $node_16_path)
+if test -n "$node_16_versions"
+	fish_add_path "$node_16_path$node_16_versions[1]/bin"
 end
 
 # openjdk
-set -l open_jdk (ls "/opt/homebrew/Cellar/openjdk/")
-if test -n "$open_jdk"
-	fish_add_path "$open_jdk[1]/bin"
+set -l open_jdk_path "/opt/homebrew/Cellar/openjdk/"
+set -l open_jdk_versions (ls $open_jdk_path)
+if test -n "$open_jdk_versions"
+	fish_add_path "$open_jdk_path$open_jdk_versions[1]/bin"
 end
