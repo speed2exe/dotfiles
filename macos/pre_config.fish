0c5596bb28611588f4b050de1e543dac406c1de2
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-15 01:53:59 +0800

    simplify logic

--- macos/pre_config.fish
+++ macos/pre_config.fish
@@ -2,15 +2,7 @@
 fish_add_path "/opt/homebrew/bin"
 
 # node@16
-set -l node_16_path "/opt/homebrew/Cellar/node@16/"
-set -l node_16_versions (ls $node_16_path)
-if test -n "$node_16_versions"
-	fish_add_path "$node_16_path$node_16_versions[1]/bin"
-end
+fish_add_path "/opt/homebrew/opt/node@16/bin"
 
 # openjdk
-set -l open_jdk_path "/opt/homebrew/Cellar/openjdk/"
-set -l open_jdk_versions (ls $open_jdk_path)
-if test -n "$open_jdk_versions"
-	set -x JAVA_HOME "$open_jdk_path$open_jdk_versions[1]"
-end
+set -x JAVA_HOME "/opt/homebrew/opt/openjdk/bin"
