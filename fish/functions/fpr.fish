6dbf5fe3fe6b5db715cbc26c88945e701e537ec7
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-17 00:01:05 +0800

    rework s function

--- fish/functions/fpr.fish
+++ fish/functions/fpr.fish
@@ -1,3 +1,3 @@
-function fpr --wraps='fp --layout reverse --height 30%' --description 'alias fpr fp --layout reverse --height 30%'
-  fp --layout reverse --height 30% $argv; 
+function fpr
+    fp --layout reverse --height 50% $argv
 end
