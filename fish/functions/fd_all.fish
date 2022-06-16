76451bc72805ad5a0605d2ea841f937af176803a
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-17 00:01:05 +0800

    rework s function

--- fish/functions/fd_all.fish
+++ fish/functions/fd_all.fish
@@ -1,3 +1,3 @@
-function fd_all --wraps='fd --hidden --no-ignore --color=always' --description 'alias fd_all fd --hidden --no-ignore --color=always'
-  fd --hidden --no-ignore --color=always $argv; 
+function fd_all
+    fd --hidden --no-ignore --color=always $argv
 end
