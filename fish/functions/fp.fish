ff2e73af091e49ee9a7ae75e1010a0233ea39a14
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-16 23:14:03 +0800

    update fp: smart case

--- fish/functions/fp.fish
+++ fish/functions/fp.fish
@@ -1,6 +1,6 @@
 function fp
 	fzf \
-	--ansi -i --preview 'echo "show {}" | fish' $argv \
+	--ansi --preview 'echo "show {}" | fish' $argv \
 	--bind ctrl-d:preview-page-down \
 	--bind ctrl-u:preview-page-up \
 	--bind ctrl-j:preview-down \
