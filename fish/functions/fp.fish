0e0211de45134b1a666815118170aafce9e81600
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-16 23:12:47 +0800

    update fp to use keybindings

--- fish/functions/fp.fish
+++ fish/functions/fp.fish
@@ -1,3 +1,10 @@
-function fp --wraps=fzf\ --ansi\ -i\ --preview\ \'echo\ \"show\ \{\}\"\ \|\ fish\' --description alias\ fp\ fzf\ --ansi\ -i\ --preview\ \'echo\ \"show\ \{\}\"\ \|\ fish\'
-  fzf --ansi -i --preview 'echo "show {}" | fish' $argv; 
+function fp
+	fzf \
+	--ansi -i --preview 'echo "show {}" | fish' $argv \
+	--bind ctrl-d:preview-page-down \
+	--bind ctrl-u:preview-page-up \
+	--bind ctrl-j:preview-down \
+	--bind ctrl-k:preview-up \
+	--bind tab:down \
+	--bind btab:up
 end
