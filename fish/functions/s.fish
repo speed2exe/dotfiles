0f81ba9ee3cc635fe88566abe8b8bffbd581cea1
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-17 00:41:49 +0800

    add prompt for s.fish for files

--- fish/functions/s.fish
+++ fish/functions/s.fish
@@ -1,9 +1,14 @@
 function s
-    set path (fd_all | fpr)
+    set path (fd_all $argv | fpr)
 
     if test -d "$path"
         cd "$path"
     else if test -f "$path"
-        nvim "$path"
+        cd (dirname "$path")
+        set filename (basename "$path")
+        prompt "edit $filename?"
+        if test $status -eq 0
+            nvim "$filename"
+        end
     end
 end
