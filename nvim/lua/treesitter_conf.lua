275a69bbe6d99e40efe5104ca242e44946f48bbd
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-18 20:20:09 +0800

    added incremental selection for nvim treesitter

--- nvim/lua/treesitter_conf.lua
+++ nvim/lua/treesitter_conf.lua
@@ -4,13 +4,18 @@ require'nvim-treesitter.configs'.setup {
     ignore_install = {"phpdoc"},
     highlight = {
         enable = true,
-		disable = {},
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
     },
     indent = {
         enable = false,
     },
     incremental_selection = {
         enable = true,
+        keymaps = {
+            init_selection = "vv",
+            node_incremental = "a",
+            scope_incremental = "r",
+            node_decremental = "u",
+        }
     },
 }
