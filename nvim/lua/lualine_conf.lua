0bd961936a366026abb7fedf37657fbd912a9cb3
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-18 22:37:55 +0800

    update lualine to use nvim-navic

--- nvim/lua/lualine_conf.lua
+++ nvim/lua/lualine_conf.lua
@@ -1,29 +1,21 @@
-require'lualine'.setup {
+local navic = require('nvim-navic')
+require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = {
-            left = '|',
-            right = '|',
-        },
-        section_separators = {
-            left = '',
-            right = '',
-        },
-        disabled_filetypes = {}
+        component_separators = { left = '|', right = '|', },
+        section_separators = { left = '', right = '', },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = true
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {
-            'branch', 'diff', {'diagnostics', sources={'nvim_diagnostic'}},
-        },
-        lualine_c = {
-            'filename',
-            'LspStatus',
-        },
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', { 'diagnostics', sources = {'nvim_diagnostic'} }, },
+        lualine_c = { 'filename', { navic.get_location, cond = navic.is_available } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
     },
     tabline = {},
     extensions = {'nvim-tree'}
--- nvim/lua/nvim-navic_conf.lua
+++ nvim/lua/nvim-navic_conf.lua
@@ -30,7 +30,7 @@ navic.setup {
         TypeParameter = " ",
     },
     highlight = false,
-    separator = " > ",
+    separator = " | ",
     depth_limit = 0,
     depth_limit_indicator = "..",
 }
