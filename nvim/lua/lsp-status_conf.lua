e3e2d41d987bfb351fa365d5521e0fc63d92be16
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-19 00:19:32 +0800

    added but commented lspstatus

--- nvim/lua/lsp-status_conf.lua
+++ nvim/lua/lsp-status_conf.lua
@@ -1,12 +1,14 @@
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
 
-local lsp_status = require('lsp-status')
-for _, server in pairs(servers) do
-    lspconfig[server].setup({
-        handers = lsp_status.extensions[server].setup(),
+-- lsp_status.config.capabilities = vim.tbl_extend('keep', lsp_status.config.capabilities or {}, lsp_status.capabilities)
+
+local lspconfig = require('lspconfig')
+local servers = require("nvim-lsp-installer").get_installed_servers()
+
+for _, server in ipairs(servers) do
+    lspconfig[server.name].setup {
         on_attach = lsp_status.on_attach,
         capabilities = lsp_status.capabilities,
-    })
+    }
 end
-
