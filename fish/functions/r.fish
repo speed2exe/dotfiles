71487f8ed651a1c56b3f8fb6253d56ade1f0fe44
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-06-23 13:49:36 +0800

    Update r.fish

--- fish/functions/r.fish
+++ fish/functions/r.fish
@@ -3,10 +3,11 @@ function r
 	set dir_hash (string replace --all / : $pwd)
 	if count $argv > /dev/null
 		command nvim $argv
-	else
-		set path (tac ~/marks/dir_hash/$dir_hash/file_history.txt | fpr)
-		if test $status -eq 0
-			nvim $path
-		end
-	end
+        return
+    end
+
+    set path (tac ~/marks/dir_hash/$dir_hash/file_history.txt | fpr)
+    if test $status -eq 0
+        nvim $path
+    end
 end
