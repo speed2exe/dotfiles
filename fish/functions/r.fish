847b3748588ecbf22940518614bc404961f60b19
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-25 18:21:37 +0800

    remove double quotes for touch and mkdir in r.fish

--- fish/functions/r.fish
+++ fish/functions/r.fish
@@ -1,14 +1,15 @@
 function r
     set pwd (pwd)
     set dir_hash (string replace --all / : $pwd)
+    echo "$dir_hash"
 
     if count $argv > /dev/null
         command nvim $argv
         return
     end
 
-    mkdir -p "~/marks/dir_hash/$dir_hash/"
-    touch "~/marks/dir_hash/$dir_hash/file_history.txt"
+    mkdir -p ~/marks/dir_hash/$dir_hash/
+    touch ~/marks/dir_hash/$dir_hash/file_history.txt
 
     set path (tac ~/marks/dir_hash/$dir_hash/file_history.txt \
         | xargs -I {} realpath --relative-to=. {} 2> /dev/null | fpr)
