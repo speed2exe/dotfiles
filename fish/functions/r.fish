b2f0fab3991bf223734a4df242f51c4d37d8eaa0
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-25 16:19:29 +0800

    improve r to filter out invalid files

--- fish/functions/r.fish
+++ fish/functions/r.fish
@@ -1,15 +1,17 @@
 function r
     set pwd (pwd)
     set dir_hash (string replace --all / : $pwd)
+
     if count $argv > /dev/null
         command nvim $argv
         return
     end
 
+    mkdir -p "~/marks/dir_hash/$dir_hash/"
     touch "~/marks/dir_hash/$dir_hash/file_history.txt"
 
-    set path (tac "~/marks/dir_hash/$dir_hash/file_history.txt" \
-        | xargs -I {} realpath --relative-to=. {} | fpr)
+    set path (tac ~/marks/dir_hash/$dir_hash/file_history.txt \
+        | xargs -I {} realpath --relative-to=. {} 2> /dev/null | fpr)
 
     if test $status -eq 0
         nvim $path
