e3eede358f26442fd0607d0e94b6bff2bc269ba8
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-06-27 01:10:02 +0800

    Update quick.lua

--- nvim/lua/quick.lua
+++ nvim/lua/quick.lua
@@ -1,1 +1,9 @@
+M = {}
 
+local toggle_virtual_text = false
+function M.toggle_virtual_text()
+    toggle_virtual_text = not toggle_virtual_text
+    vim.diagnostic.config({virtual_text = toggle_virtual_text})
+end
+
+return M
