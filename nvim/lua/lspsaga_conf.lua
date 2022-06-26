3a6c938aee3d9bb85df63b218720fb884ef509fd
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-06-27 00:23:30 +0800

    Update lspsaga_conf.lua

--- nvim/lua/lspsaga_conf.lua
+++ nvim/lua/lspsaga_conf.lua
@@ -1,2 +1,19 @@
 local saga = require 'lspsaga'
-saga.init_lsp_saga()
+saga.init_lsp_saga{
+    finder_action_keys = {
+        open = '<CR>',
+        vsplit = 'v',
+        split = 'x',
+        quit = '<ESC>',
+        scroll_down = '<C-D>',
+        scroll_up = '<C-U>'
+    },
+    code_action_keys = {
+        quit = '<ESC>',
+        exec = '<CR>'
+    },
+    rename_action_keys = {
+        quit = '<C-C>',
+        exec = '<CR>'
+    },
+}
