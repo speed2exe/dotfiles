78738c847baf43c56537254ffb13572c8885f52c
Fu Zi Xiang <33050391+speed2exe@users.noreply.github.com>
2022-06-27 00:50:45 +0800

    Update v.fish

--- fish/functions/v.fish
+++ fish/functions/v.fish
@@ -1,10 +1,25 @@
 function v
 	if count $argv > /dev/null
-		nvim $argv
+		set path "$argv"
 	else
 		set path (tac ~/marks/file_history.txt | fpr)
 		if test $status -eq 0
 			nvim $path
 		end
 	end
+
+    if test -z "$path"
+        return
+    end
+
+    set splitted (string split --max 1 --right ":" "$path") # account for line number if exists
+    set file "$splitted[1]"
+    set line "$splitted[2]"
+
+    if test -n "$line"
+        nvim "$file" "+$line"
+        return
+    end
+
+    nvim $file
 end
