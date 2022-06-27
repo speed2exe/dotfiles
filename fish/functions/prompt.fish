282a106feaa2798002086c32e8d3055c5913cf4e
Fu Zi Xiang <speed2exe@live.com.sg>
2022-06-27 22:57:23 +0800

    use one char for y/n

--- fish/functions/prompt.fish
+++ fish/functions/prompt.fish
@@ -1,6 +1,6 @@
 function prompt
     set question "$argv"
-    set answer (string trim (read --line --prompt-str "$question (y/n) "))
+    set answer (string trim (read -n 1 --prompt-str "$question (y/n) "))
     if test "$answer" = "y"
         return 0
     end
