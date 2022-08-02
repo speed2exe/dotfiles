72cd07e06b63e699451a3481e97ba31c37496d7b
Fu Zi Xiang <speed2exe@live.com.sg>
2022-08-02 22:59:11 +0800

    touch stuff before reading

--- fish/functions/d.fish
+++ fish/functions/d.fish
@@ -1,3 +1,4 @@
 function d
+    touch ~/marks/directories.txt
     set dir (cat ~/marks/directories.txt | fpr)'/' && cd "$dir"
 end
--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -1,3 +1,4 @@
 function f
+    touch ~/marks/files.txt
     set file (cat ~/marks/files.txt | fpr) && nvim "$file"
 end
