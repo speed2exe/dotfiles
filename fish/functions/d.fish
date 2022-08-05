327d4044afdcdde18c5ce008089d0c9ec8709212
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-08-05 18:02:23 +0800

    remove self initiatad file and folder search

--- fish/functions/d.fish
+++
@@ -1,4 +0,0 @@
-function d
-    touch ~/marks/directories.txt
-    set dir (cat ~/marks/directories.txt | fpr)'/' && cd "$dir"
-end
--- fish/functions/f.fish
+++
@@ -1,4 +0,0 @@
-function f
-    touch ~/marks/files.txt
-    set file (cat ~/marks/files.txt | fpr) && nvim "$file"
-end
