12624560a76ce94c86774d63af944d6de480f69b
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-08-15 21:20:58 +0800

    added missing files

---
+++ nvim/lua/nvim-lsp-installer_setup.lua
@@ -0,0 +1,14 @@
+local vim = vim
+local servers = require("nvim-lsp-installer").get_installed_servers()
+local lspconfig = require("lspconfig")
+local navic = require("nvim-navic")
+local cmp = require('cmp_nvim_lsp')
+
+for _, server in ipairs(servers) do
+    lspconfig[server.name].setup {
+        on_attach = function(client, bufnr)
+            navic.attach(client, bufnr)
+        end,
+        capabilities = cmp.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    }
+end
