506e3be2f5ceac4792ced368824dd802e67cb383
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-08-15 17:30:42 +0800

    use only 1 file for lsp servers setup

--- nvim/lua/nvim-navic_conf.lua
+++ nvim/lua/nvim-navic_conf.lua
@@ -34,14 +34,3 @@ navic.setup {
     depth_limit = 0,
     depth_limit_indicator = "..",
 }
-
-local servers = require("nvim-lsp-installer").get_installed_servers()
-local lspconfig = require("lspconfig")
-
-for _, server in ipairs(servers) do
-    lspconfig[server.name].setup {
-        on_attach = function(client, bufnr)
-            navic.attach(client, bufnr)
-        end,
-    }
-end
