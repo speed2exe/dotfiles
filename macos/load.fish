274014824a2f8a4d9794d240c0e42dd1e9a67d8e
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-08-21 21:36:57 +0800

    increase font size to 16 for macos

--- macos/load.fish
+++ macos/load.fish
@@ -31,5 +31,6 @@ printf '\nkey_bindings:
 sed -i -e 's/iosevka2/Iosevka\ Nerd\ Font/g' ~/.config/alacritty/alacritty.yml
 sed -i -e 's/xdg\-open/open/g' ~/.config/alacritty/alacritty.yml
 sed -i -e 's/\/bin\/fish/\/opt\/homebrew\/bin\/fish/g' ~/.config/alacritty/alacritty.yml
+sed -i -e 's/12.0/16.0/g' ~/.config/alacritty/alacritty.yml
 
 cp ~/dotfiles/macos/neovide.vim ~/.config/nvim/neovide.vim
