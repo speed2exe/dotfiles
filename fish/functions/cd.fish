aaf6cad8aa11c97eb371eb9824df1b4fe9e2136a
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-03 22:02:01 +0800

    rework n.fish

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -2,13 +2,14 @@ function cd
     set prev_git (git rev-parse --show-toplevel 2> /dev/null)
     builtin cd "$argv"
     set cur_git (git rev-parse --show-toplevel 2> /dev/null)
+    set cur_path "$PWD/"
 
     # display git info if in different git repo
     test "$cur_git" && test "$prev_git" != "$cur_git" && onefetch
 
     # make sure file exists
     touch /tmp/dir_history
-    echo "$PWD/" >> /tmp/dir_history
+    echo $cur_path >> /tmp/dir_history
 
     # print the current directory and the items in it
     echo; print_boxed "$cur_path"; echo; la
