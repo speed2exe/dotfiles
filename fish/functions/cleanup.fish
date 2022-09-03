bbec1affafae83cd5d7e3d2ae7073fc37e0a93ba
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-03 22:48:44 +0800

    limit to 1000 lines for directory history

--- fish/functions/cleanup.fish
+++ fish/functions/cleanup.fish
@@ -1,4 +1,7 @@
 function cleanup
-    cat /tmp/dir_history >> ~/marks/dir_history.txt
+    cat ~/marks/dir_history.txt > /tmp/dir_history_temp
+    cat /tmp/dir_history >> /tmp/dir_history_temp
+    tail -n 1000 /tmp/dir_history_temp > ~/marks/dir_history.txt
     rm /tmp/dir_history
+    rm /tmp/dir_history_temp
 end
