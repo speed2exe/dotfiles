c1ab916345c5ec09dea5131f906c59714912e8c6
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-09-06 17:34:28 +0800

    nvim tree use numbers

--- nvim/lua/nvim-tree_conf.lua
+++ nvim/lua/nvim-tree_conf.lua
@@ -57,8 +57,8 @@ require'nvim-tree'.setup {
 		hide_root_folder = false,
 		side = 'left',
 		preserve_window_proportions = false,
-		number = false,
-		relativenumber = false,
+		number = true,
+		relativenumber = true,
 		signcolumn = "yes",
 		mappings = {
 			custom_only = true,
