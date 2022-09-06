f26462c475fdf1fbb3b1425e848fed0b30a1c1e9
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-09-06 10:35:41 +0800

    readjust dir_history

--- fish/functions/cleanup.fish
+++ fish/functions/cleanup.fish
@@ -1,7 +1,7 @@
 function cleanup
     cat ~/marks/dir_history.txt > /tmp/dir_history_temp
     cat /tmp/dir_history >> /tmp/dir_history_temp
-    tail -n 1000 /tmp/dir_history_temp > ~/marks/dir_history.txt
+    tail -n 1000 /tmp/dir_history_temp | awk '!x[$0]++' > ~/marks/dir_history.txt
     rm /tmp/dir_history
     rm /tmp/dir_history_temp
 end
