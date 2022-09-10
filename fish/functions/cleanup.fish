dd89fae415775686aa74a71cb7deae400beab5f9
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-10 19:11:29 +0800

    fixed dir_history.txt not updating properly

--- fish/functions/cleanup.fish
+++ fish/functions/cleanup.fish
@@ -1,7 +1,7 @@
 function cleanup
     cat ~/marks/dir_history.txt > /tmp/dir_history_temp
     cat /tmp/dir_history >> /tmp/dir_history_temp
-    tail -n 1000 /tmp/dir_history_temp | awk '!x[$0]++' > ~/marks/dir_history.txt
+    tac /tmp/dir_history_temp | tail -n 1000 | awk '!x[$0]++' | tac > ~/marks/dir_history.txt
     rm /tmp/dir_history
     rm /tmp/dir_history_temp
 end
