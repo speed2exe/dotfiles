887205c5906fc86121bc6b98beaaf9c7a800f9b3
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-17 00:54:20 +0800

    improve z

--- fish/functions/bind_ctrl_z.fish
+++ fish/functions/bind_ctrl_z.fish
@@ -1,3 +1,14 @@
 function bind_ctrl_z
+    set items (jobs)
+    if test -z "$items"
+        return
+    end
+
+    if test (count $items) -eq 1
+        fg (echo "$items" | cut -f 2)
+    end
+
     set job (jobs | fr | cut -f 2) && fg $job
+
+    commandline --function repaint-mode
 end
