7203e1474a30c723549269eddb31a6a82c0682c3
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-17 01:13:42 +0800

    improve z and q

--- fish/functions/bind_ctrl_q.fish
+++ fish/functions/bind_ctrl_q.fish
@@ -2,4 +2,6 @@ function bind_ctrl_q
     for j in (jobs | cut -f 2)
         kill $j && disown $j 2> /dev/null &
     end
+
+    commandline --function repaint
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,18 @@
 function fish_prompt
     set last_status $status
+
+    set job_list (jobs)
+    if success
+        set_color -b brblack black ; printf " $(count $job_list) " ; set_color normal
+    end
+
     starship prompt
+
     if test $last_status -eq 0
-        set_color -b green black ; printf " " ; set_color normal
+        set_color -b green ; printf " " ; set_color normal
     else
         set_color -b red black ; printf " $last_status " ; set_color normal
     end
+
     printf " "
 end
