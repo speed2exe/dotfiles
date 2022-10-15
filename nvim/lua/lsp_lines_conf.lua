fe7ba059f266e886078bdcad16b6d5b6c6c73f95
Fu Zi Xiang <speed2exe@live.com.sg>
2022-10-16 02:04:24 +0800

    update vim config

--- nvim/lua/lsp_lines_conf.lua
+++ nvim/lua/lsp_lines_conf.lua
@@ -2,13 +2,11 @@ M = {}
 
 require('lsp_lines').setup()
 
-vim.diagnostic.config({virtual_text = false})
-
-local inline_text = true
+local use_lines = false
 function M.toggle_inline_text()
-    inline_text = not inline_text
-    vim.diagnostic.config({virtual_lines = inline_text})
-    vim.diagnostic.config({virtual_text = not inline_text})
+    vim.diagnostic.config({virtual_text = use_lines})
+    use_lines = not use_lines
+    vim.diagnostic.config({virtual_lines = use_lines})
 end
 
 return M
