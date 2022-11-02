b8389b094f915f4709a72bba0e3033c345be1f10
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-11-02 12:36:57 +0800

    nvim: lua: lsp: simplify setup process

--- nvim/lua/nvim-lspconfig_setup.lua
+++ nvim/lua/nvim-lspconfig_setup.lua
@@ -6,10 +6,7 @@ local cmp = require('cmp_nvim_lsp')
 
 for _, server in ipairs(servers) do
     lspconfig[server].setup {
-        root_dir = lspconfig.util.root_pattern('.git'),
-        on_attach = function(client, bufnr)
-            navic.attach(client, bufnr)
-        end,
+        on_attach = navic.attach,
         capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     }
 end
