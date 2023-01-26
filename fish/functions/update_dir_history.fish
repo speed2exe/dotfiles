93434703de2efb5f0d0253121222ea154b03de70
Fu Zi Xiang <speed2exe@live.com.sg>
2023-01-27 04:10:53 +0800

    fish update last used directory impl

--- fish/functions/update_dir_history.fish
+++ fish/functions/update_dir_history.fish
@@ -1,5 +1,5 @@
 function update_dir_history
     cat ~/marks/dir_history.txt /tmp/dir_history > /tmp/buffer
     tac /tmp/buffer | tail -n 1000 | awk '!x[$0]++' | tac > ~/marks/dir_history.txt
-    rm /tmp/buffer
+    rm /tmp/buffer /tmp/dir_history
 end
