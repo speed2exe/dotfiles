3b514e41a2bd5c9b8cdd3a64e317196eca7266a0
Fu Zi Xiang <speed2exe@live.com.sg>
2023-01-27 12:53:29 +0800

    fish: f to use Telescope find_files, c to go to a directory fast

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -1,14 +1,19 @@
 function cd
+    # previous git root
     set prev_git (git rev-parse --show-toplevel 2> /dev/null)
+
+    # actually change directory
     builtin cd $argv || return
+
+    # print the current directory and the items in it
+    print_boxed "$PWD"; la
+
+    # current git root
     set cur_git (git rev-parse --show-toplevel 2> /dev/null)
-    set cur_path "$PWD/"
 
     # display git info if in different git repo
     test "$cur_git" && test "$prev_git" != "$cur_git" && onefetch 2> /dev/null
 
-    echo $cur_path >> /tmp/dir_history
-
-    # print the current directory and the items in it
-    print_boxed "$cur_path"; la
+    # update directory history
+    echo "$PWD/" >> /tmp/dir_history
 end
