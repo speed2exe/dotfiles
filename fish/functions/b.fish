9d702e4cdee2b99650591bdfe8bda4ef2849ff01
Fu Zi Xiang <speed2exe@live.com.sg>
2023-02-02 00:53:51 +0800

    update fish: removed unneeded stuff

--- fish/functions/b.fish
+++
@@ -1,6 +0,0 @@
-# disown all jobs let them run on their own in background
-function b
-    for j in (jobs | cut -f 2)
-        disown $j
-    end
-end
--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,8 +1,5 @@
 function fish_greeting
-    if test -f /tmp/.cmdln
-        set content (cat /tmp/.cmdln)
-        printf $content | xclip -sel clip
-        set_color yellow; echo "'$content' is in clipboard"
-        rm /tmp/.cmdln
-    end
+    echo
+    set_color brblack ; fortune ; set_color normal
+    echo
 end
