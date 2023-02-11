2c571fc261f0522ad6161a593815e5a1f81d3398
Fu Zi Xiang <speed2exe@live.com.sg>
2023-02-11 17:22:04 +0800

    nvim: update lualine

--- nvim/lua/nvim-lspconfig_setup.lua
+++ nvim/lua/nvim-lspconfig_setup.lua
@@ -9,6 +9,25 @@ for _, server in ipairs(servers) do
         root_dir = lspconfig.util.root_pattern('.git'),
         on_attach = function(client, bufnr)
             navic.attach(client, bufnr)
+
+            -- https://sbulav.github.io/til/til-neovim-highlight-references/
+            if client.server_capabilities.documentHighlightProvider then
+                vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+                vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+                vim.api.nvim_create_autocmd("CursorHold", {
+                    callback = vim.lsp.buf.document_highlight,
+                    buffer = bufnr,
+                    group = "lsp_document_highlight",
+                    desc = "Document Highlight",
+                })
+                vim.api.nvim_create_autocmd("CursorMoved", {
+                    callback = vim.lsp.buf.clear_references,
+                    buffer = bufnr,
+                    group = "lsp_document_highlight",
+                    desc = "Clear All the References",
+                })
+            end
+
         end,
         capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     }
