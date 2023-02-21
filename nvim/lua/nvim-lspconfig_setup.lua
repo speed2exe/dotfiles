e8a9194aa25adf9dd5fd0f4871feca545bd44809
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-02-22 02:56:11 +0800

    added formatting on save

--- nvim/lua/nvim-lspconfig_setup.lua
+++ nvim/lua/nvim-lspconfig_setup.lua
@@ -27,6 +27,11 @@ for _, server in ipairs(servers) do
                     desc = "Clear All the References",
                 })
             end
+
+            -- autoformat on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                callback = function() vim.lsp.buf.format() end,
+            })
         end,
         capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     }
