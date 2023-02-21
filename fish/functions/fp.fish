d5e33eadb6b9b274473afcdd6842b4e88ce8a082
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-02-22 05:05:07 +0800

    fzf preview half-page

--- fish/functions/fp.fish
+++ fish/functions/fp.fish
@@ -6,8 +6,8 @@ function fp
         --preview-window 'top,50%,border-bottom' \
         --preview 'fish -c "show {}"' $argv \
         --bind 'tab:change-preview-window(right,50%,border-left|top,50%,border-bottom)' \
-        --bind ctrl-d:preview-page-down \
-        --bind ctrl-u:preview-page-up \
+        --bind ctrl-d:preview-half-page-down \
+        --bind ctrl-u:preview-half-page-up \
         --bind ctrl-j:preview-down \
         --bind ctrl-k:preview-up \
         -i $argv
