e820e9ce4e54f192a833d56f42ff1c029251606f
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-02-22 06:56:14 +0800

    disable blankline indent context start

--- nvim/lua/indent-blankline_conf.lua
+++ nvim/lua/indent-blankline_conf.lua
@@ -3,7 +3,7 @@ vim.opt.list = true
 
 require("indent_blankline").setup {
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
     use_treesitter = true,
     use_treesitter_scope = true,
 }
