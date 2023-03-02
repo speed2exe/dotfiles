3ff134597f5295d0f13190b07e26a0e4b45fc422
Fu Zi Xiang <speed2exe@live.com.sg>
2023-03-02 23:23:25 +0800

    improve man usability

--- fish/functions/man.fish
+++ fish/functions/man.fish
@@ -1,5 +1,5 @@
 function man
-    command man "$argv" | ansifilter > /tmp/.man
-    and cat /tmp/.man | nvim -c "set syntax=man"
+    command man "$argv" > /tmp/.man
+    and cat /tmp/.man | ansifilter | nvim -c "set syntax=man"
     rm /tmp/.man
 end
