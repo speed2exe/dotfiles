55dda8586479f5865d3558337787a2128d0d081b
Fu Zi Xiang <speed2exe@live.com.sg>
2023-03-05 00:28:33 +0800

    rework fzf

--- fish/functions/navigate.fish
+++ fish/functions/navigate.fish
@@ -6,22 +6,11 @@ function navigate
         return
     end
 
-    set dir (realpath "$argv")"/"
-    set path (fd_all "$argv" --absolute-path --exact-depth 1 | fprp --query "$dir")
+    set dir (realpath "$argv")
+    fd_all "$argv" --absolute-path --exact-depth 1 | fp \
+        --query "$dir" \
+        --bind "right:become(cd {} && navigate)" \
+        --bind "left:become(cd .. && navigate)" \
+        --bind "esc:print-query" \
 
-    set input "$path[1]"
-    set selection "$path[2]"
-
-    if test (string match "*.." "$input") # user inputs ".."
-        navigate "$input"
-    else if test -d "$selection"
-        navigate "$selection"
-    else if test -f "$selection"
-        printf "$selection"
-    else if test -n "$input"
-        error "no such file or directory: $input"
-        navigate "$dir"
-    else if test -z "$selection"
-        printf "$dir"
-    end
 end
