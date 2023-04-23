96cc79545b4d87bcbc923036810162cacda68600
Fu Zi Xiang <speed2exe@live.com.sg>
2023-04-24 01:35:39 +0800

    fish:function:v: support column

--- fish/functions/v.fish
+++ fish/functions/v.fish
@@ -7,13 +7,18 @@ function v
 
     test -z "$path" && return
 
-    set splitted (string split --max 1 --right ":" "$path") # account for line number if exists
+    set splitted (string split --max 2 --right ":" "$path") # account for line number if exists
     set file "$splitted[1]"
     set line "$splitted[2]"
+    set column "$splitted[3]"
 
-    if test -n "$line"
+    if test -n "$column"
+        nvim "$file" "+call cursor($line, $column)"
+    else if test -n "$line"
         nvim "$file" "+$line"
-    else
+    else if test -f "$file"
         nvim "$file"
+    else
+        error "File not found: $file"
     end
 end
