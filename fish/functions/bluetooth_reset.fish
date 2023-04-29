078160f1faf005ab837258a17ffc54012e5141e4
Fu Zi Xiang <speed2exe@live.com.sg>
2023-04-29 23:42:27 +0800

    added quick notify

--- fish/functions/bluetooth_reset.fish
+++ fish/functions/bluetooth_reset.fish
@@ -2,7 +2,7 @@ function bluetooth_reset
     systemctl restart bluetooth.service
     sleep 1
 
-    bluetoothctl devices | while read -l a b c 
+    bluetoothctl devices | while read -l a b c
         bluetoothctl connect $b
     end
 end
