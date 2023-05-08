13656954a6f260da4ab1fdab04f0bf3792e55a9f
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-05-08 12:09:19 +0800

    update nvim webdevicon

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,14 @@
 -- comment this line if you set up for the first time
 require('impatient')
 
-require('plugins') -- ~/.config/nvim/lua/plugins.lua
-require('keymap') -- ~/.config/nvim/lua/keymap.lua
+require('plugins')  -- ~/.config/nvim/lua/plugins.lua
+require('keymap')   -- ~/.config/nvim/lua/keymap.lua
 require('settings') -- ~/.config/nvim/lua/settings.lua
 
 -- custom stuff
-require('last_used') -- ~/.config/nvim/lua/last_used.lua
+require('last_used')            -- ~/.config/nvim/lua/last_used.lua
 require('nvim-lspconfig_setup') -- ~/.config/nvim/lua/nvim-lspconfig_setup.lua
-require('autocmd') -- ~/.config/nvim/lua/autocmd.lua
+require('autocmd')              -- ~/.config/nvim/lua/autocmd.lua
 
 vim.cmd [[
     " https://stackoverflow.com/questions/630884/opening-vim-help-in-a-vertical-split-window
