c0455a766db939f3b63334b666b7ad739bab9255
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-05-10 14:18:04 +0800

    update telescope

--- nvim/lua/telescope_conf.lua
+++ nvim/lua/telescope_conf.lua
@@ -17,12 +17,6 @@ require('telescope').setup {
         },
     },
     extensions = {
-        fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        },
         file_browser = {
             hijack_netrw = true,
             hidden = true,
