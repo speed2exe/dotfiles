f9a2963cd4cd39b40c39b5567c2b086e166d071f
Fu Zi Xiang <speed2exe@live.com.sg>
2023-06-02 20:52:56 +0800

    cmp: use super tab for snippy

--- nvim/lua/nvim-cmp_conf.lua
+++ nvim/lua/nvim-cmp_conf.lua
@@ -1,14 +1,40 @@
 -- Setup nvim-cmp.
 local cmp = require('cmp')
 local lspkind = require('lspkind')
+local snippy = require('snippy')
 lspkind.init()
+
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
     mapping = {
         ['<C-U>'] = cmp.mapping.scroll_docs(-4),
         ['<C-D>'] = cmp.mapping.scroll_docs(4),
-        ['<TAB>'] = cmp.mapping.select_next_item(),
-        ['<S-TAB>'] = cmp.mapping.select_prev_item(),
+        ["<TAB>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif snippy.can_expand_or_advance() then
+                snippy.expand_or_advance()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-TAB>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif snippy.can_jump(-1) then
+                snippy.previous()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
         ['<DOWN>'] = cmp.mapping.select_next_item(),
         ['<UP>'] = cmp.mapping.select_prev_item(),
         ['<C-E>'] = cmp.mapping.abort(),
