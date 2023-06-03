b1c1b147113813ddbde14aaa5baaf840ddbf7ddb
Fu Zi Xiang <speed2exe@live.com.sg>
2023-06-03 16:23:15 +0800

    simplify dir history 2

--- fish/functions/n.fish
+++ fish/functions/n.fish
@@ -1,6 +1,7 @@
 function n
-    set dir (tac ~/marks/dir_history.txt | tee /tmp/dir_history.txt | fp --no-sort)
+    tac ~/marks/dir_history.txt | awk '!x[$0]++' > /tmp/dir_history.txt
+    set dir (cat /tmp/dir_history.txt | fp --no-sort)
     and builtin cd $dir
-    and tac /tmp/dir_history.txt | awk '!x[$0]++' > ~/marks/dir_history.txt
+    and tac /tmp/dir_history.txt > ~/marks/dir_history.txt
     and echo $dir >> ~/marks/dir_history.txt
 end
