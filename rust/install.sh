e3f40909784c0e35c7e0efa64a912eb32bf1e9e9
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-04 18:07:30 +0800

    added docker-compose and postgres

--- rust/install.sh
+++ rust/install.sh
@@ -8,6 +8,7 @@ cp -r ~/.ssh .ssh
 docker build -t rust-img .
 rm .gitconfig
 rm -r .ssh
-docker rm -f rust-cont || true
-docker run --name rust-cont -d rust-img
-docker exec -it rust-cont bash
+# docker rm -f rust-cont || true
+# docker run --add-host=host.docker.internal:host-gateway --name rust-cont -d rust-img
+# docker exec -it rust-cont fish
+docker-compose up -d
