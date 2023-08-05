41a47d67c4c94b32a9fa6e0561dcb056de0d9711
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-05 16:03:21 +0800

    fish: t.fish: improve usability

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -5,7 +5,5 @@ function t
         v (basename "$path")
     else if test -d "$path"
         cd "$path"
-    else
-        set_color red; echo "No such file or directory"; set_color normal
     end
 end
