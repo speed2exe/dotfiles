2bf6d5d35ff550632a7d6efc7cf0fc5ea9b0cef6
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-13 10:50:07 +0800

    improve neovide replacement for nvim

--- fish/functions/nvim.fish
+++ fish/functions/nvim.fish
@@ -1,6 +1,12 @@
 function nvim
     if type -q neovide
-        command neovide $argv
+        if test -z "$argv"
+            command neovide
+        else if string match -- '-*' $argv[1] > /dev/null
+            command neovide -- $argv
+        else
+            command neovide $argv[1] -- $argv[2..]
+        end
     else
         command nvim $argv
     end
