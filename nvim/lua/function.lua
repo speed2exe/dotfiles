1668731b1744453ce4274e4ea012340b2c1a5304
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-16 22:00:17 +0800

    nvim: remove unneeded stuff

--- nvim/lua/function.lua
+++ nvim/lua/function.lua
@@ -3,22 +3,6 @@
 local M = {}
 local vim = vim
 
-M.toggle_quick_fix_list = function()
-    local qf_exists = false
-    for _, win in pairs(vim.fn.getwininfo()) do
-        if win["quickfix"] == 1 then
-            qf_exists = true
-        end
-    end
-    if qf_exists == true then
-        vim.cmd "cclose"
-        return
-    end
-    if not vim.tbl_isempty(vim.fn.getqflist()) then
-        vim.cmd "copen"
-    end
-end
-
 vim.diagnostic.config({ virtual_lines = false })
 local init = false
 M.toggle_lsp_lines = function()
--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -7,9 +7,7 @@ local baleia = require('baleia')
 vim.g.mapleader = ' ';
 
 -- Interpret ansi colors
-set('n', '<leader>cl', function()
-    baleia.setup().once(vim.fn.bufnr())
-end)
+set('n', '<leader>cl', function() baleia.setup().once(vim.fn.bufnr()) end)
 
 -- create file if not exists
 set('n', '<leader>e', ':e <cfile><CR>')
@@ -24,16 +22,13 @@ set('n', '<leader>v', '`[v`]')
 set('v', '<leader>s', ':s/<C-R>"/<C-R>"')
 
 -- copy current file path to clipboard
-set('n', 'yf', '<CMD>let @+ = expand("%:p")<CR>')
+set('n', '<leader>yf', '<CMD>let @+ = expand("%:p")<CR>')
 -- copy current file path with line number to clipboard
-set('n', 'yF', '<CMD>let @+ = join([expand("%:p"), line(".")], ":")<CR>')
+set('n', '<leader>yl', '<CMD>let @+ = join([expand("%:p"), line(".")], ":")<CR>')
 -- copy current file directory to clipboard
-set('n', 'yd', '<CMD>let @+ = expand("%:p:h")<CR>')
+set('n', '<leader>yd', '<CMD>let @+ = expand("%:p:h")<CR>')
 -- yank current file content to clipboard
-set('n', 'y%', '<CMD>:%y+<CR>')
-
--- toggle quickfix window
-set('n', '<C-Q>', fn.toggle_quick_fix_list)
+set('n', '<leader>yy', '<CMD>:%y+<CR>')
 
 -- go to project root
 set('n', '<leader>pr', '<CMD>ProjectRoot<CR>')
@@ -49,21 +44,17 @@ set('n', '<leader>j', t_builtin.jumplist)
 set('n', '<leader>q', t_builtin.quickfix)
 set('n', '<leader>rg', t_builtin.registers)
 set('n', '<leader>m', t_builtin.marks)
-set('n', '<leader>ff', t_builtin.find_files)
+set('n', '<leader>f', t_builtin.find_files)
 set('n', '<leader>gf', t_builtin.git_files)
-set('n', '<leader>lg', t_builtin.live_grep) -- search
+set('n', '<leader>s', t_builtin.live_grep) -- search
 set('n', '<leader>ls', t_builtin.lsp_dynamic_workspace_symbols)
 set('n', '<leader>o', t_builtin.oldfiles)
 set('n', '<leader>b', t_builtin.buffers)
 
--- Toggle indent-blankline to show tabs
-set('n', '<C-B>', ':IndentBlanklineToggle<CR>')
-
 -- Toggle LSP diagnostics
-set('n', '<leader>ll', fn.toggle_lsp_lines)
+set('n', '<leader>dt', fn.toggle_lsp_lines) -- diagnostics line
 
 -- Custom Personal Mapping
--- Add more if needed, but shouldn't need more
 set('n', '<leader>rn', vim.lsp.buf.rename)
 set('n', '<leader>df', vim.lsp.buf.definition)
 set('n', '<leader>i', vim.lsp.buf.implementation)
@@ -71,7 +62,7 @@ set('n', '<leader>rf', vim.lsp.buf.references)
 set('n', '<leader>hv', vim.lsp.buf.hover)
 set('n', '<leader>a', vim.lsp.buf.code_action)
 set('n', '<leader>t', vim.lsp.buf.type_definition)
-set('n', '<leader>fm', vim.lsp.buf.format)
-set('n', '<leader>dl', vim.lsp.buf.document_highlight)
+set('n', '<leader>n', vim.lsp.buf.format) -- neat
+set('n', '<leader>hl', vim.lsp.buf.document_highlight)
 set('n', '<leader>cr', vim.lsp.buf.clear_references)
 set('n', '<leader>do', vim.diagnostic.open_float)
