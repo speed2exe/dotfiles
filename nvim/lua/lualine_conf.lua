44f7aea0b254e6b198ac4b78e361d1258caa70b1
Fu Zi Xiang <speed2exe@live.com.sg>
2023-08-18 20:07:37 +0800

    rework lualine_conf

--- nvim/lua/lualine_conf.lua
+++ nvim/lua/lualine_conf.lua
@@ -1,3 +1,4 @@
+local vim = vim
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
 local function get_lsp_status()
@@ -12,40 +13,44 @@ local function cwd()
     return vim.fn.getcwd()
 end
 
+local function user()
+    return vim.env.USER
+end
+
 require('lualine').setup {
     options = {
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
     },
     winbar = {
-        lualine_a = { { cwd } },
-        lualine_b = { { 'filename', path = 1 } },
-        lualine_c = { 'location' },
-        lualine_x = { 'progress' },
-        lualine_y = {},
+        lualine_a = { 'filetype' },
+        lualine_b = { cwd },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { user },
+        lualine_y = { 'hostname' },
         lualine_z = {},
     },
     inactive_winbar = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { { cwd }, { 'filename', path = 1 }, 'location' },
-        lualine_x = { 'progress' },
+        lualine_c = { 'filetype', cwd, { 'filename', path = 1 } },
+        lualine_x = { user, 'hostname' },
         lualine_y = {},
         lualine_z = {},
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'filetype' },
-        lualine_c = { { get_lsp_status, cond = lsp_is_availabale }, 'diagnostics' },
-        lualine_x = { 'diff', 'branch' },
-        lualine_y = {},
+        lualine_b = { 'location' },
+        lualine_c = { 'progress' },
+        lualine_x = { { get_lsp_status, cond = lsp_is_availabale }, 'diagnostics' },
+        lualine_y = { 'diff', 'branch' },
         lualine_z = {},
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'mode', 'filetype', { get_lsp_status, cond = lsp_is_availabale }, 'diagnostics' },
-        lualine_x = { 'diff', 'branch' },
+        lualine_c = { 'mode', 'location', 'progress' },
+        lualine_x = { { get_lsp_status, cond = lsp_is_availabale }, 'diagnostics', 'diff', 'branch' },
         lualine_y = {},
         lualine_z = {},
     },
