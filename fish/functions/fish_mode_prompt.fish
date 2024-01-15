f46df8c38b3be1d34fcaa97d54f915e847f532af
Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-15 15:42:43 +0800

    feat: update config

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,14 +1,12 @@
 function fish_mode_prompt
 	switch $fish_bind_mode
-    case "visual"
-        set_color -b magenta; printf " " ; set_color normal;
-	case "insert"
-        set_color -b green; printf " " ; set_color normal; echo -en "\e[6 q"
-	case "default"
-        set_color -b blue; printf " " ; set_color normal; echo -en "\e[2 q"
-	case "replace"
-        set_color -b yellow; printf " " ; set_color normal;
-    case "*"
-        set_color -b white black; printf " $fish_bind_mode "; set_color normal;
+		case "visual"
+			set_color black -b magenta; printf " " ; set_color normal;
+		case "insert"
+			set_color black -b green; printf " " ; set_color normal;
+		case "replace"
+			set_color black -b yellow; printf " " ; set_color normal;
+		case "*"
+			set_color black -b blue; printf " "; set_color normal;
 	end
 end
