ea1c98b1960746154d00681813dc3ad7ee0a18c7
Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-23 01:07:00 +0800

    add curl for easy oneliner

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# curl https://raw.githubusercontent.com/speed2exe/dotfiles/main/setup.sh | bash
+
+cd ~
+git clone https://github.com/speed2exe/dotfiles.git --depth=1
+cd dotfiles
+./load
+cd
