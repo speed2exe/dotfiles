670439d2fb2515360f8b5db6c82c30dbcc85a7da
Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-24 00:19:05 +0800

    update fish and lsp

--- fish/functions/bind_ctrl_r.fish
+++ fish/functions/bind_ctrl_r.fish
@@ -1,9 +1,11 @@
 function bind_ctrl_r
     set path (fd_all . $argv | fp)
-    if test -d $path
-	cd $path
-	commandline --function repaint
-    else if test -f $path
-	$EDITOR $path
+    if success
+        if test -d $path
+            cd $path
+            commandline --function repaint
+        else if test -f $path
+            $EDITOR $path
+        end
     end
 end
