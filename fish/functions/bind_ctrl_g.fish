d766a6f67be74fd4edaf70d5c2139f6520afe8f9
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-25 00:24:47 +0800

    revamp lsp remove mason

--- fish/functions/bind_ctrl_g.fish
+++ fish/functions/bind_ctrl_g.fish
@@ -2,6 +2,4 @@ function bind_ctrl_g
     set git_dir (git rev-parse --show-toplevel 2> /dev/null)
     and cd "$git_dir"
     and commandline --function repaint
-    and return
-    or error "current directory is not a git repository or in a git repository"
 end
