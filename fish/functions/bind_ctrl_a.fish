a4f067fa3111165bfe310e36663900424a781473
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-25 03:09:53 +0800

    ctrl_a to drop to nix shell with fish

--- fish/functions/bind_ctrl_a.fish
+++ fish/functions/bind_ctrl_a.fish
@@ -1,5 +1,4 @@
 function bind_ctrl_a
-    set path (fd_all --type directory . $argv | fp)
-    and cd $path
-    and commandline --function repaint
+    nix-shell --command "fish"
+    commandline --function repaint
 end
