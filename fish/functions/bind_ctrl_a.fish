fce92b4b5766f927e5eab8519bf5209a2275e0ff
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-25 17:57:36 +0800

    nix shell integration

--- fish/functions/bind_ctrl_a.fish
+++ fish/functions/bind_ctrl_a.fish
@@ -1,5 +1,5 @@
 function bind_ctrl_a
-    nix-shell --command "fish"
+    nix-shell --command "fish --init-command='set -ax NIX_SHELL_STACK (pwd)'"
     or echo
     commandline --function repaint
 end
