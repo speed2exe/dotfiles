61cff7979a5709e5495f4722322bf2a9aefde733
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-26 11:23:22 +0800

    restore ctrl_a

--- fish/functions/bind_ctrl_a.fish
+++ fish/functions/bind_ctrl_a.fish
@@ -1,5 +1,5 @@
 function bind_ctrl_a
-    nix-shell --command "fish --init-command='set -ax NIX_SHELL_STACK (pwd)'"
-    or echo
-    commandline --function repaint
+    set path (fd_all --type directory . $argv | fp)
+    and cd $path
+    and commandline --function repaint
 end
