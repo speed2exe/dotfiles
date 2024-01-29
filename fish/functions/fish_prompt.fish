46d116efd2f84490e14021c8b05c0bc7a9c9756f
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-29 15:24:49 +0800

    remove unneeded fansy stuff

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,6 @@
 function fish_prompt
     set last_status $status
 
-    if test -n "$NIX_SHELL_STACK"
-	set COUNT (echo "$NIX_SHELL_STACK" | wc --words)
-	set_color -b blue black ; printf " $COUNT " ; set_color normal
-    end
-
     set job_list (jobs)
     if success
         set_color -b brblack black ; printf " $(count $job_list) " ; set_color normal
