91118e3b9d87fed5ff13fc05fd39190c5ea52895
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-30 18:45:05 +0800

    revamp ctrl n and ctrl a

--- fish/functions/bind_ctrl_a.fish
+++ fish/functions/bind_ctrl_a.fish
@@ -1,5 +1,10 @@
 function bind_ctrl_a
-    set path (fd_all --type directory . $argv | fp)
-    and cd $path
-    and commandline --function repaint
+    set dir (tac ~/marks/git_dir_history.txt \
+      | awk '!x[$0]++' \
+      | tee /tmp/git_dir_history.txt \
+      | fp --no-sort)
+    tac /tmp/git_dir_history.txt > ~/marks/git_dir_history.txt
+
+    test -n "$dir" && cd $dir
+    commandline --function repaint
 end
