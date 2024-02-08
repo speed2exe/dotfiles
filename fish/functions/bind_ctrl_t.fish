684dabab5dcde54bb02efd36ace63914bdecf7d4
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-02-08 23:16:11 +0800

    feat: remove telescope

--- fish/functions/bind_ctrl_t.fish
+++ fish/functions/bind_ctrl_t.fish
@@ -1,11 +1,11 @@
 function bind_ctrl_t
-    set path (navigate $argv) # ~/.config/fish/functions/navigate.fish
-    if test -f "$path"
-        cd (dirname "$path")
-	commandline --function repaint
-        nvim (basename "$path")
-    else if test -d "$path"
-        cd "$path"
-	commandline --function repaint
-    end
+  set path (navigate $argv) # ~/.config/fish/functions/navigate.fish
+  if test -f "$path"
+    cd (dirname "$path")
+    commandline --function repaint
+    nvim (basename "$path")
+  else if test -d "$path"
+    cd "$path"
+    commandline --function repaint
+  end
 end
