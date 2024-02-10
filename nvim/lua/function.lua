c898486917a1101177bc3378c78fe7ccd0368713
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-02-10 22:32:46 +0800

    add quickfix toggle

--- nvim/lua/function.lua
+++ nvim/lua/function.lua
@@ -18,4 +18,14 @@ M.toggle_lsp_lines = function()
     })
 end
 
+M.toggle_quickfix = function()
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      vim.cmd "cclose"
+      return
+    end
+  end
+  vim.cmd "copen"
+end
+
 return M
