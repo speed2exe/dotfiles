270066f9b24510221bc250e4491be94c834708f4
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-02-12 15:44:06 +0800

    improve keys

--- fish/functions/bind_ctrl_o.fish
+++ fish/functions/bind_ctrl_o.fish
@@ -1,6 +1,6 @@
 function bind_ctrl_o
   nvim -c "redir! > /tmp/vimoldfiles | silent oldfiles | redir END | q" > /dev/null
-  set path (sed '1d' /tmp/vimoldfiles | awk -F ": " '{print $2 ":" $1}' | rg '^/' | fp)
+  set path (sed '1d' /tmp/vimoldfiles | awk -F ": " '{print $2}' | rg '^/' | fp)
   if success
     v $path
   end
