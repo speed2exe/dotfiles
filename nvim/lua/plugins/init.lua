5560b570f5b6578725a27b2c8946463c4a4bd15c
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-02-23 20:36:15 +0800

    updates

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,50 +1,66 @@
+local vim = vim
 return {
-	'github/copilot.vim',
-	{ "folke/which-key.nvim", config = function()
-		require('which-key').setup()
-	end },
-	'm00qek/baleia.nvim',
-	'onsails/lspkind-nvim',
-	{ 'Mofiqul/dracula.nvim',
-	config = function() vim.cmd('colorscheme dracula')
-	end },
-	{ 'lewis6991/gitsigns.nvim', config = function()
-		require('gitsigns').setup()
-	end },
-	{ 'nvim-tree/nvim-web-devicons', config = function()
-		require('nvim-web-devicons').setup()
-	end },
-	'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-	'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+  'github/copilot.vim',
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require('which-key').setup()
+    end
+  },
+  'm00qek/baleia.nvim',
+  'onsails/lspkind-nvim',
+  {
+    'Mofiqul/dracula.nvim',
+    config = function()
+      vim.cmd('colorscheme dracula')
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('nvim-web-devicons').setup()
+    end
+  },
+  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
 
-	-- Telescope stuff
-	-- 'nvim-lua/popup.nvim',
-	-- 'nvim-lua/plenary.nvim',
-	-- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	-- 'nvim-telescope/telescope-file-browser.nvim',
+  -- Telescope stuff
+  -- 'nvim-lua/popup.nvim',
+  -- 'nvim-lua/plenary.nvim',
+  -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  -- 'nvim-telescope/telescope-file-browser.nvim',
 
-	-- LSP stuff
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-	'neovim/nvim-lspconfig',
-	'nvim-lua/lsp-status.nvim',
+  -- LSP stuff
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  'neovim/nvim-lspconfig',
+  'nvim-lua/lsp-status.nvim',
 
-	-- TreeSitter stuff
-	'nvim-treesitter/nvim-treesitter-refactor',
-	{ 'nvim-treesitter/nvim-treesitter-context', config = function()
-		require('treesitter-context').setup()
-	end },
+  -- TreeSitter stuff
+  'nvim-treesitter/nvim-treesitter-refactor',
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup()
+    end
+  },
 
-	-- Cmp stuff
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/cmp-nvim-lsp-signature-help',
-	'hrsh7th/cmp-nvim-lsp-document-symbol',
-	'hrsh7th/cmp-nvim-lua',
-	'hrsh7th/cmp-cmdline',
-	'hrsh7th/cmp-buffer',
-	'hrsh7th/cmp-path',
-	'mtoohey31/cmp-fish',
-	'ray-x/cmp-treesitter',
-	'dcampos/nvim-snippy',
-	'dcampos/cmp-snippy',
+  -- Cmp stuff
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
+  'hrsh7th/cmp-nvim-lsp-document-symbol',
+  'hrsh7th/cmp-nvim-lua',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'mtoohey31/cmp-fish',
+  'ray-x/cmp-treesitter',
+  'dcampos/nvim-snippy',
+  'dcampos/cmp-snippy',
 }
