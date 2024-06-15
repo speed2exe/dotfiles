0e90a80c59944f65a10427360bac3a745324e196
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-06-15 14:03:29 +0800

    refactor

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
 # curl https://raw.githubusercontent.com/speed2exe/dotfiles/main/setup.sh | bash
 
-cd ~
 git clone https://github.com/speed2exe/dotfiles.git --depth=1
-cd dotfiles
-./load
-cd
+dotfiles/sync
