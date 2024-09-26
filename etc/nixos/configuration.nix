8690f31c6226a40f3fee6b964ade1a6466c7647e
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-09-26 20:25:15 +0800

    use latest linux

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -9,6 +9,7 @@
   # Boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Network
   networking.networkmanager.enable = true;
