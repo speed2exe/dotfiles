582e9ccceb55669f6ea864ee125a82956b2a5795
zack2827 <speed2exe@live.com.sg>
2025-02-05 19:16:08 +0800

    added asahi linux config

---
+++ asahi/etc/nixos/flake.nix
@@ -0,0 +1,21 @@
+{
+
+  inputs = {
+    # nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    unstable.url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    # persway.url = "https://github.com/johnae/persway/archive/main.tar.gz";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "aarch64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; inherit system; };
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+}
