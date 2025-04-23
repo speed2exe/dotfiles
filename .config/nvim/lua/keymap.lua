8172a1bd3c2971adc187b2f6e1d2d3fabc83e640
Fu Zi Xiang <fuzi.xiang@bytedance.com>
2025-04-23 14:28:04 +0800

    improve nvim shell

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -86,8 +86,8 @@ end)
 set('n', '<leader>l', vim.lsp.buf.format)
 set('n', '<leader>r', vim.lsp.buf.rename)
 set('n', '<leader>a', vim.lsp.buf.code_action)
-set('n', '<leader>e', ':term ')
-set('v', '<leader>e', ':w! /tmp/nvim-shell-cmd.sh<CR>:term source /tmp/nvim-shell-cmd.sh<CR>')
+set('n', '<leader>e', ':term bash -i -c \'\'<LEFT>')
+set('v', '<leader>e', ':w! /tmp/nvim-shell-cmd.sh<CR>:term bash -i < /tmp/nvim-shell-cmd.sh 2> /dev/null<CR>')
 set('n', '<leader>u', '<CMD>lcd %:p:h<CR>')     -- go to current file directory
 set('n', '<leader>x', fn.open_term_at_file_dir) -- open interactive terminal at current file directory
 
