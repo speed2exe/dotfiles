dd591e1ba7f24b6748c3cf931576f6b6cd7ca75d
Fu Zi Xiang <zixiang.fu@bytedance.com>
2026-02-20 12:25:18 +0800

    fix macos paste not working

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -2,3 +2,7 @@ vim.g.neovide_opacity = 0.5
 vim.g.transparency = 0.5
 vim.g.neovide_refresh_rate = 120
 vim.o.guifont = "IosevkaTerm Nerd Font:h14"
+
+-- https://github.com/neovide/neovide/issues/1602
+vim.keymap.set("c", "<D-v>", "<C-r>+")
+vim.keymap.set("i", "<D-v>", "<C-r>+")
