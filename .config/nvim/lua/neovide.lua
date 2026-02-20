1b6445c5e4149aa434cebea1848f95c8e13384bd
Fu Zi Xiang <zixiang.fu@bytedance.com>
2026-02-20 16:05:39 +0800

    color contrast

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -1,5 +1,5 @@
-vim.g.neovide_opacity = 0.5
-vim.g.transparency = 0.5
+vim.g.neovide_opacity = 0.7
+vim.g.transparency = 0.3
 vim.g.neovide_refresh_rate = 120
 vim.o.guifont = "IosevkaTerm Nerd Font:h14"
 
